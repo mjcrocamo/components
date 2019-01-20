b5d154ae92e3c9519629694a358e48f92960898a
Maggie Crocamo <mjcrocamo@gmail.com>
2019-01-20 13:23:27 -0500

    changes to files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// semantic-ui.com, semantic ui cdn
+// faker js -- generates  fake data for projects
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div className="ui container comments">
+        <ApprovalCard>
+            <h4>Warning!</h4>
+                Are you sure you want to do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4 pm" 
+                    comment="Nice!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Mary" 
+                    timeAgo="Today at 5 pm" 
+                    comment="Woot!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="SpiderMan"  
+                    timeAgo="Yesterday at 3 pm" 
+                    comment="Hellosh"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
